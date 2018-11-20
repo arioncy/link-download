a3bbf4f96854e9f11a2a52ce939e4c104a2f13fe
arioncy <arioncy8@gmail.com>
2018-11-20 11:58:57 +0700

    Update link-downlaod-movie.js

--- link-downlaod-movie.js
+++ link-downlaod-movie.js
@@ -1,10 +1,6 @@
 <script type='text/javascript'>
 function findlik(){
-    if (id == '121')
-	{
+  
         window.open('google.com','_blank');
-	}
-    else if(id == 'NULL')
-	{ 
-	}
+	
 </script>
